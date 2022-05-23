41143c8f96814103da6c8b05b52227a5aa29be65
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-05-23 23:53:11 +0200

    add search controller in search screen

--- Spotify/Controllers/Core/SearchViewController.swift
+++ Spotify/Controllers/Core/SearchViewController.swift
@@ -8,11 +8,38 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    //MARK: - Properties
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: SearchResultsViewController())
+        searchController.searchBar.placeholder = "Songs, Artists, Albums"
+        searchController.searchBar.searchBarStyle = .minimal
+        searchController.definesPresentationContext = true
+        return searchController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
         
     }
 
+}
+
+//MARK: - UISearchResultsUpdating
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchResultsVC = searchController.searchResultsController as? SearchResultsViewController,
+              let query = searchController.searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        print(query)
+        // here will perform searching
+        // Api Calling 
+    }
+    
+    
 }
---
+++ Spotify/Controllers/Others/SearchResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewController.swift
+//  Spotify
+//
+//  Created by Ahmed Amin on 23/05/2022.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+     
+    }
+
+}
